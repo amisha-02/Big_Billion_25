f91a7ccf80798df97589c9b64c1ba94b99f59d89
Harsh Raj <82524165+harshmanu22900@users.noreply.github.com>
2022-06-26 01:57:56 +0530

    connect the frontend to commerce.js

---
+++ App.js
@@ -0,0 +1,37 @@
+import React,{useState,useEffect} from 'react'
+//import Products from './components/Products/Products';
+//import Navbar from './components/Navbar/Navbar';
+import {commerce}from './lib/commerce';
+import {Products,Navbar} from './components';
+const App = () => {
+    const[products,setProducts]= useState([]); 
+    const [cart, setCart] = useState({});
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+    
+        setProducts(data);
+    }
+    //API call;
+    const fetchCart = async () => {
+         setCart ( await commerce.cart.retrieve());
+       }
+       const handleAddToCart = async(productId,quantity)=>{
+         const item = await commerce.cart.add(productId,quantity);
+         setCart(item.cart)
+       }
+  useEffect(()=>{
+      fetchProducts();
+      fetchCart();
+ },[]);
+ console.log(cart);
+ 
+  return (
+    <div>
+        <Navbar totalItems={cart.total_items}/>
+        <Products products={products} onAddToCart = {handleAddToCart} />
+
+    </div>
+  )
+}
+
+export default App
